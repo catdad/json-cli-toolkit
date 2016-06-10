c5af4a86475a72f6123d0b90afe269a61a96ff8a
Kiril Vatev <vatev.1@gmail.com>
2016-06-09 20:10:40 -0400

    adding pluck tests

---
+++ test/pluck.test.js
@@ -0,0 +1,26 @@
+/* jshint node: true, mocha: true */
+
+var expect = require('chai').expect;
+
+var pluck = require('../lib/pluck.js');
+
+describe('[pluck]', function() {
+    it('returns the property defined by attr', function() {
+        var out = pluck({
+            thing: 'stuff'
+        }, {
+            attr: 'thing'
+        });
+        
+        expect(out).to.equal('stuff');
+    });
+    it('can return nested properties', function() {
+        var out = pluck({
+            nested: { key: 'value' }
+        }, {
+            attr: 'nested.key'
+        });
+        
+        expect(out).to.equal('value');
+    });
+});
