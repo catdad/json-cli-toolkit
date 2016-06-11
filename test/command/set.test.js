30e40af2e2c333a3889cad70c258adf7a99ac165
Kiril Vatev <vatev.1@gmail.com>
2016-06-11 11:16:43 -0400

    adding set command
    #4

---
+++ test/command/set.test.js
@@ -0,0 +1,66 @@
+/* jshint node: true, mocha: true */
+
+var expect = require('chai').expect;
+var _ = require('lodash');
+
+var set = require('../../lib/command/set.js');
+
+describe('[set]', function() {
+    it('returns the object with a new valu set', function() {
+        var OBJ = { one: 2 };
+        
+        var val = set(OBJ, {
+            attr: 'three',
+            value: 4
+        });
+        
+        expect(OBJ).to.equal(val);
+        expect(val).to.have.property('three').and.to.equal(4);
+    });
+    
+    it('can set falsy values', function() {
+        var OBJ = { one: 2 };
+        
+        var val = set(OBJ, {
+            attr: 'three',
+            value: false
+        });
+        
+        expect(OBJ).to.equal(val);
+        expect(val).to.have.property('three').and.to.equal(false);
+    });
+    
+    it('transforms the string "null" to the value null', function() {
+        var OBJ = { one: 2 };
+        
+        var val = set(OBJ, {
+            attr: 'three',
+            value: 'null'
+        });
+        
+        expect(OBJ).to.equal(val);
+        expect(val).to.have.property('three').and.to.equal(null);
+    });
+    
+    it('returns the object unmodified if attr is not set', function() {
+        var OBJ = { one: 2 };
+        var CLONE = _.clone(OBJ);
+        
+        var val = set(CLONE, {
+            value: 4
+        });
+        
+        expect(CLONE).to.deep.equal(OBJ);
+    });
+
+    it('returns the object unmodified if value is not set', function() {
+        var OBJ = { one: 2 };
+        var CLONE = _.clone(OBJ);
+        
+        var val = set(CLONE, {
+            attr: 'three',
+        });
+        
+        expect(CLONE).to.deep.equal(OBJ);
+    });
+});
