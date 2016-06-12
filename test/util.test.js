a704ad5a5c69bcb0853654ddcb5729437513438a
Kiril Vatev <vatev.1@gmail.com>
2016-06-12 10:07:14 -0400

    adding ability to ignore non-json lines instead of erroring
    #1

--- test/util.test.js
+++ test/util.test.js
@@ -36,5 +36,27 @@ describe('[util]', function() {
             
             input.end(DATA);
         });
+        
+        it('ignored bad json with a flag', function(done) {
+            var DATA = JSON.stringify({ example: 'pants' });
+            var input = through();
+            var t = util.transform(input, {
+                ignore: true
+            });
+            
+            ns.wait(t, function(err, buff) {
+                if (err) {
+                    return done(err);
+                }
+                
+                expect(buff.toString()).to.equal(DATA);
+                
+                done();
+            });
+            
+            input.write(DATA);
+            input.write('not json data');
+            input.end();
+        });
     });
 });
