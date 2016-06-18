8fd589202a85f0ef68a2582466d842227cbc5f63
Kiril Vatev <vatev.1@gmail.com>
2016-06-18 17:21:37 -0400

    adding more wrap tests
    #9

--- test/command/wrap.test.js
+++ test/command/wrap.test.js
@@ -29,4 +29,21 @@ describe('[wrap]', function() {
             .to.have.property('name')
             .and.to.equal(OBJ);
     });
+    
+    it('can create an array', function() {
+        var OBJ = {};
+        var opts = {
+            attr: 'name[0]',
+            argv: {}
+        };
+        
+        expect(wrap(OBJ, opts))
+            .to.have.property('name')
+            .and.to.be.an('array')
+            .and.to.have.lengthOf(1)
+            // not sure if it is a good idea to test
+            // arrays this way, but it works
+            .to.have.property('0')
+            .and.to.equal(OBJ);    
+    });
 });
