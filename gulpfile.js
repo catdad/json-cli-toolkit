b4a7396a930c2bae6d6dd5db825426a9c468dca5
Kiril Vatev <vatev.1@gmail.com>
2017-02-05 12:51:48 -0500

    adding gulp test task

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var mocha = require('gulp-mocha');
 
-var source = {
-  js: ['*.js', 'bin/**/*.js', 'lib/**/*.js', 'test/**/*.js'],
-  lib: ['*.js', 'bin/**/*.js', 'lib/**/*.js']
-};
+var source = (function () {
+  var src = {};
+
+  src.lib = ['*.js', 'bin/**/*.js', 'lib/**/*.js'];
+  src.test = ['test/**/*.js'];
+  src.js = [].concat(src.lib).concat(src.test);
+
+  return src;
+}());
 
 gulp.task('lint', function () {
   return gulp.src(source.js)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', function () {
+  return gulp.src(source.test)
+    .pipe(mocha());
 });
