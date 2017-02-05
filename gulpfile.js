c9c0cd4ec85b677e69962eb2dc1487804dfbd1a5
Kiril Vatev <vatev.1@gmail.com>
2017-02-05 16:04:28 -0500

    abstracting out the graceful error gulp wrapper into a separate file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
-var gulp = require('gulp');
+var gulp = require('./gulp-graceful.js')(require('gulp'));
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
 var sequence = require('gulp-sequence');
 var istanbul = require('gulp-istanbul');
-var gutil = require('gulp-util');
 var argv = require('yargs')
   .boolean('lint')
   .default('lint', true)
@@ -23,71 +22,12 @@ var source = (function () {
   return src;
 }());
 
-// no clue if this is a good idea or not yet
-var wrapped = (function (g) {
-  function wrap(stream) {
-    var gracefulError = false;
-
-    var pipe = stream.pipe.bind(stream);
-    var emit = stream.emit.bind(stream);
-
-    // wrap all streams at the time that they are piped in
-    stream.pipe = function (destStream, opts) {
-      return pipe(wrap(destStream), opts);
-    };
-
-    // overload emit function in case this stream is allowed
-    // to ignore errors in the future
-    stream.emit = function (name, err) {
-      if (name !== 'error' || gracefulError === false) {
-        return emit.apply(null, arguments);
-      }
-
-      var logErr = new gutil.PluginError('wrapper', err);
-
-      gutil.log(logErr.toString());
-
-      // set the exit code, so that whenever the build
-      // exits, it does so with an error
-      process.exitCode = 1;
-
-      // emit a regular end event, since an error is supposed
-      // to end the stream
-      return emit('end');
-    };
-
-    // when calling continue, we will allow this stream
-    // to swallow errors, and instead set the exitCode
-    // for when the build eventually exits
-    stream.graceful = function (val) {
-      gracefulError = typeof val === 'boolean' ? val : true;
-
-      return stream;
-    };
-
-    return stream;
-  }
-
-  // this is where gulp builds start, so wrap the
-  // resulting stream, allowing all streams in the
-  // pipeline to be wrapped as well
-  g.src = (function (src) {
-    return function () {
-      var stream = src.apply(null, arguments);
-
-      return wrap(stream);
-    };
-  }(g.src.bind(g)));
-
-  return g;
-}(gulp));
-
 gulp.task('lint', function () {
   if (!argv.lint) {
     return null;
   }
 
-  return wrapped.src(source.js)
+  return gulp.src(source.js)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
@@ -109,7 +49,7 @@ gulp.task('coverage-report', function () {
     return null;
   }
 
-  return wrapped.src(source.test)
+  return gulp.src(source.test)
     .pipe(istanbul.writeReports())
     .pipe(istanbul.enforceThresholds({
       thresholds: {
@@ -121,7 +61,7 @@ gulp.task('coverage-report', function () {
 });
 
 gulp.task('mocha', function () {
-  return wrapped.src(source.test)
+  return gulp.src(source.test)
     .pipe(mocha())
     .graceful(!argv.fast);
 });
