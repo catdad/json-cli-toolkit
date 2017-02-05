eb57dd3b55e349e1cee50c40cee90e8b9a4349f5
Kiril Vatev <vatev.1@gmail.com>
2017-02-05 14:38:59 -0500

    adding gulp tasks for test and coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,49 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var mocha = require('gulp-mocha');
+var sequence = require('gulp-sequence');
+var istanbul = require('gulp-istanbul');
+var argv = require('yargs')
+  .default('lint', true)
+  .argv;
 
 var source = (function () {
   var src = {};
 
-  src.lib = ['*.js', 'bin/**/*.js', 'lib/**/*.js'];
+  src.lib = ['toolkit.js', 'bin/**/*.js', 'lib/**/*.js'];
   src.test = ['test/**/*.js'];
-  src.js = [].concat(src.lib).concat(src.test);
+  src.js = ['*.js'].concat(src.lib).concat(src.test);
 
   return src;
 }());
 
 gulp.task('lint', function () {
+  if (!argv.lint) {
+    return null;
+  }
+
   return gulp.src(source.js)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', function () {
+gulp.task('coverage-instrument', function () {
+  return gulp.src(source.lib)
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('coverage-report', function () {
+  return gulp.src(source.test)
+    .pipe(istanbul.writeReports());
+});
+
+gulp.task('mocha', function () {
   return gulp.src(source.test)
     .pipe(mocha());
 });
+
+gulp.task('coverage', sequence('coverage-instrument', 'mocha', 'coverage-report'));
+
+gulp.task('test', sequence('lint', 'coverage'));
