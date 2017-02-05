d8cada0a9860b9d6e899e0bfae9f4d5d5ff1f323
Kiril Vatev <vatev.1@gmail.com>
2017-02-05 12:17:50 -0500

    manually fixing a bunch of files

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
 var source = {
-    js: ['*.js', 'bin/**/*.js', 'lib/**/*.js', 'test/**/*.js']
+  js: ['*.js', 'bin/**/*.js', 'lib/**/*.js', 'test/**/*.js']
 };
 
 gulp.task('lint', function () {
-    return gulp.src(source.js)
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+  return gulp.src(source.js)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
